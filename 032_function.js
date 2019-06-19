4821c759403a08b2b146e05856e7a943d747812c
newseoyun <seoyuntab25@gmail.com>
2019-06-20 00:34:09 +0900

    d

---
+++ 032_function.js
@@ -0,0 +1,23 @@
+
+// Function Expression
+
+var canadaaa = function() {  
+    console.log('cold');
+}
+
+var canada = () => {  // ES6
+    console.log('cold');
+}
+
+
+// Function Declaration
+
+function india() {
+    console.log('warm');
+}
+
+
+// Function Invocation/Call/Execution
+
+canada()
+india()
---
+++ 033_argument.js
@@ -0,0 +1,27 @@
+
+function marry(person1, person2) {
+    console.log(arguments);
+    console.log(Array.from(args));
+    return `${person1} is now married to ${person2}`
+}
+
+marry('Tina', 'Tom');
+
+/* 
+arguments는 parameters와 비슷하지만 좀 다름. 혼용해서 쓰기도 함.
+단 argument는 인수, parameter는 매개변수로, 
+매개변수는 임의의 이름으로 정의할 수 있지만(이름이 있지만) 
+인수는 이름이 없다.
+엄밀히 말하면 위에서 person1,2는 매개변수이고, tina tom은 인수임.
+JS에서 arguments는 배열로 저장된다. 
+그래서 아래와같이 매우 편리하게 접근할 수 있다.
+*/
+
+
+function marry2(...args) {
+    console.log(arguments);
+    console.log(Array.from(args));
+    return `${args[0]} is now married to ${args[1]}`
+}
+
+marry2('Max', 'Lisa');
