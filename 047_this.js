29ef7b51740d0703ce73281f0f3f096967e8d3ab
Seoyun Lee <newseoyun@gmail.com>
2021-08-26 16:24:10 +0900

    Update 047_this.js
    this와 화살표함수에대해 더 공부하고 설명 업데이트

--- 047_this.js
+++ 047_this.js
@@ -4,6 +4,7 @@ var b = {
     name : 'jay',
     say() { console.log(this) }
 }
+// b 객체 리턴
 
 var c = {
     name : 'jay',
@@ -14,8 +15,10 @@ var c = {
 var d = {
     name : 'jay',
     say() { return () => console.log(this) }
-    // d.say()() 이거는 에로우펑션이 현 스코프 내부에서 동작하기때문에 d를 리턴함.
+    // d.say()() 이거는 에로우펑션이 현 스코프 내부에서 동작하기때문에 d를 리턴함. (lexical scope)
 }
 
-// 에로우펑션은 현재 스코프에서만 동작한다는 사실을 기억할 것
-//  즉, 스코프를 한정함
+
+// 화살표함수는 자기 자신의 this가 없다. 
+// 함수마다 this가 바인딩 되던 예전 버전에서는 객체지향에 안좋았다.
+// 화살표 함수에서 this를 찾으면, 자신을 둘러싸는 렉시컬 범위(lexical scope)의 this가 사용되어서 바로 바깥의 this를 가져온다.
